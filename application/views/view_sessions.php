65572604450b6500e3ea50f2235ef66ad79f127d
hayreddintuzel <hayreddintuzel@gmail.com>
2020-10-20 17:59:37 +0300

    Update view_sessions.php

--- application/views/view_sessions.php
+++ application/views/view_sessions.php
@@ -1,6 +1,8 @@
 <link href="<?= base_url() ?>assets/css/attendee-session-view.css?v=<?= rand(1, 100) ?>" rel="stylesheet">
 <style>
-
+    .player-controls__right-control-group{
+        display:none !important;
+    }
     .wrapper{
         background-color: black;
     }
@@ -610,15 +612,6 @@ if (isset($sessions)) {
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js" integrity="sha512-v8ng/uGxkge3d1IJuEo6dJP8JViyvms0cly9pnbfRxT6/31c3dRWxIiwGnMSWwZjHKOuY3EVmijs7k1jz/9bLA==" crossorigin="anonymous"></script>
 <script type="text/javascript">
-    setTimeout(function () {
-        $("head").prepend(`
-        <style>
-            .player-controls__right-control-group{
-            display:none !important;
-            }
-        </style>
-        `)
-    },2000)
     $(document).ready(function () {
     let socket = io("<?=getSocketUrl()?>");
     socket.emit("ConnectSessioViewUsers","<?=getAppName($sessions->sessions_id) ?>")
