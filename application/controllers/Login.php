e96fdd030c8839d688d302af8cd269e0338feaff
athulak <athullive@gmail.com>
2021-04-22 13:42:47 +0200

    Land on Attend page instead of View

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -116,7 +116,7 @@ class Login extends CI_Controller {
                 $this->session->set_userdata($session);
                 $sessions = $this->db->get_where('sessions', array('sessions_id' => $response_array->session));
                 if ($sessions->num_rows() > 0) {
-                    redirect('sessions/view/' . $response_array->session);
+                    redirect('sessions/attend/' . $response_array->session);
                 } else {
                     redirect('sessions');
                 }
@@ -166,7 +166,7 @@ class Login extends CI_Controller {
                     $this->session->set_userdata($session);
                     $sessions = $this->db->get_where('sessions', array('sessions_id' => $response_array->session));
                     if ($sessions->num_rows() > 0) {
-                        redirect('sessions/view/' . $response_array->session);
+                        redirect('sessions/attend/' . $response_array->session);
                     } else {
                         redirect('sessions');
                     }
